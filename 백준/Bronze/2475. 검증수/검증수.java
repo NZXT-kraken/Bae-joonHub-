68478fc662bc05f3930820e7e745ba9a3dae244c
NZXT-kraken <marblegrays24@gmail.com>
2025-03-29 16:35:27 +0900

    [Bronze V] Title: 검증수, Time: 172 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for(int i = 0; i < 5; i++) {
+            int a = sc.nextInt();
+            sum = sum + a*a;
+
+
+
+
+        }
+
+        System.out.println(sum%10);
+
+
+    }
+
+}
